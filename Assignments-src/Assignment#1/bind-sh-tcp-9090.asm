dec0fff9a02da72bd691caa81aa0479390024213
thryb <thryb@msn.com>
2016-07-14 16:37:42 -0400

    Assignment 1 asm file

---
+++ Assignments-src/Assignment#1/bind-sh-tcp-9090.asm
@@ -0,0 +1,14 @@
+;
+; Linux x86
+; Author:  thryb
+; Date:    13-07-16
+; Purpose: Bind /bin/sh to TCP port 9090
+; Size:    XX bytes
+; ID:      SLAE-770
+;
+
+global _start
+
+section .text
+_start:
+
