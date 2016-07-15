52492d1c6e1e615c3b7c3649985e4927a26208a0
thryb <thryb@msn.com>
2016-07-15 18:09:12 -0400

    Adding port2shell.py

---
+++ Assignments-src/Assignment#1/port2shell.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# 
+# Convert port to shellcode format
+# Author: thryb
+# Date: 15-07-16
+# ID: SLAE-770
+#
+
+import socket, sys
+
+if len(sys.argv) == 2:
+	l_port = int(sys.argv[1])
+else:
+	print "Usage: %s <port number>" % str(sys.argv[1])
+
+hex_port = '{0:04X}'.format(l_port)
+
+h1 = hex_port[0:2]
+h2 = hex_port[2:4]
+
+print "Replace Local port with: \n\\x%s\\x%s" % (h1, h2) 
