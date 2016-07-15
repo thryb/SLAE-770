a62936d726f9fc488427cbc83d4cd97009b11463
thryb <thryb@msn.com>
2016-07-14 22:10:51 -0400

    socket

--- Assignments-src/Assignment#1/bind-sh-tcp-9090.asm
+++ Assignments-src/Assignment#1/bind-sh-tcp-9090.asm
@@ -12,3 +12,15 @@ global _start
 section .text
 _start:
 
+	; 1 - create socket
+	; socket(AF_INET, SOCK_STREAM, 0);
+	xor eax, eax ; null aex
+	push eax ; 3rd arg of socket (0)
+	mov al, 0x66 ; sys_socketcall = 102 (http://docs.cs.up.ac.za/programming/asm/derick_tut/syscalls.html)
+	mov bl, 0x1 ; socketcall() socket = 1
+	push 0x2 ; arg (SOCK_STREAM)
+	push 0x1 ; arg (AF_INET)
+	mov ecx, esp ; mov stack ptr to ecx
+	int 0x80 ; init
+
+	; 2 - bind socket	
